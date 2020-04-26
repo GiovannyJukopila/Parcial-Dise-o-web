cd06397d4417035e425a20caa2a49e5c35478876
GiovannyJukopila <jukopila.giovanny@hotmail.com>
2020-04-25 21:47:42 -0500

    mi primer commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <div>
+        <p><span style={{color:
+           '#ffffff', fontFamily: 'georgia, palatino', fontSize: 'xx-large',backgroundColor: '#00000080'}}>Enciende el&nbsp;<span style={{fontSize: 'xx-large', fontFamily: 'impact, chicago', color: '#ff0000',backgroundColor: '#000000' }}>FUEGO</span></span></p>
+      </div>
+      <div>
+        <p><span style={{color: '#ffffff', fontFamily: 'georgia, palatino', fontSize: 'xx-large',backgroundColor: '#00000080'}}>Que hay en tu&nbsp;<span style={{fontFamily: 'impact, chicago', fontSize: 'xx-large', color: '#ff0000',backgroundColor: '#000000'}}>INTERIOR</span><span style={{color: '#ffffff', fontFamily: 'georgia, palatino', fontSize: 'x-large'}}><br /></span></span></p>
+        <p>&nbsp;</p>
+      </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
