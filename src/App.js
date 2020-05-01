33d39824b3f5afc652207ee3e34e6429c650ac2b
GiovannyJukopila <jukopila.giovanny@hotmail.com>
2020-04-30 20:13:27 -0500

    mi primer commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './App.css';
+import './gym.jpg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       <div>
-        <p><span style={{color:
-           '#ffffff', fontFamily: 'georgia, palatino', fontSize: 'xx-large',backgroundColor: '#00000080'}}>Enciende el&nbsp;<span style={{fontSize: 'xx-large', fontFamily: 'impact, chicago', color: '#ff0000',backgroundColor: '#000000' }}>FUEGO</span></span></p>
+        <p><span style={{color:'#ffffff', fontFamily: 'georgia, palatino', fontSize: 'xx-large',backgroundColor: '#00000080',border: 'ridge'}}>Enciende el&nbsp;<span style={{fontSize: '80px', fontFamily: 'impact, chicago', color: '#ff0000'}}>FUEGO</span></span></p>
       </div>
       <div>
-        <p><span style={{color: '#ffffff', fontFamily: 'georgia, palatino', fontSize: 'xx-large',backgroundColor: '#00000080'}}>Que hay en tu&nbsp;<span style={{fontFamily: 'impact, chicago', fontSize: 'xx-large', color: '#ff0000',backgroundColor: '#000000'}}>INTERIOR</span><span style={{color: '#ffffff', fontFamily: 'georgia, palatino', fontSize: 'x-large'}}><br /></span></span></p>
+        <p><span style={{color: '#ffffff', fontFamily: 'georgia, palatino', fontSize: 'xx-large',backgroundColor: '#00000080',border: 'ridge'}}>Que hay en tu&nbsp;<span style={{fontFamily: 'impact, chicago', fontSize: '80px', color: '#ff0000'}}>INTERIOR</span><span style={{color: '#ffffff', fontFamily: 'georgia, palatino', fontSize: 'x-large'}}><br /></span></span></p>
         <p>&nbsp;</p>
       </div>
       </header>
