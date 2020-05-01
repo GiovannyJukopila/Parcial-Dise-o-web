4c2e6f4ff94c3a2f5d6e4e5cbac3dbd5171b3941
GiovannyJukopila <36655827+GiovannyJukopila@users.noreply.github.com>
2020-04-30 21:08:29 -0500

    Add files via upload

---
+++ build/precache-manifest.375db64f9b294cca4f8f367f13ebd839.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "480fe8a32bac4d3c4da6c9e63c92310e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd7d66bb22ed6e390fae",
+    "url": "/static/css/main.66b3d0a9.chunk.css"
+  },
+  {
+    "revision": "152ab32a689fd07435af",
+    "url": "/static/js/2.571ec6f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.571ec6f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd7d66bb22ed6e390fae",
+    "url": "/static/js/main.b5146910.chunk.js"
+  },
+  {
+    "revision": "c5f5a680185e2b53a34b",
+    "url": "/static/js/runtime-main.da049e05.js"
+  },
+  {
+    "revision": "26f3fc2a69b3be1318e7d9d9cb7a45d9",
+    "url": "/static/media/gym.26f3fc2a.jpg"
+  }
+]);
